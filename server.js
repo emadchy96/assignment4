dc7bd70358cf772dd20212e64419508953235a54
emadchy96 <echowdhury7@myseneca.ca>
2024-07-10 23:33:03 -0400

    update

--- server.js
+++ server.js
@@ -11,7 +11,6 @@
 *
 ********************************************************************************/ 
 
-
 const express = require("express");
 const path = require("path");
 const data = require("./collegeData.js");
@@ -20,18 +19,17 @@ const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.get("/about", (req,res) => {
+app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
-app.get("/htmlDemo", (req,res) => {
+app.get("/htmlDemo", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
 });
 
@@ -51,47 +49,45 @@ app.get("/students", (req, res) => {
     }
 });
 
-app.get("/students/add", (req,res) => {
+app.get("/students/add", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/addStudent.html"));
 });
 
-
 app.post("/students/add", (req, res) => {
-    data.addStudent(req.body).then(()=>{
-      res.redirect("/students");
+    data.addStudent(req.body).then(() => {
+        res.redirect("/students");
     });
-  });
+});
 
 app.get("/student/:studentNum", (req, res) => {
     data.getStudentByNum(req.params.studentNum).then((data) => {
         res.json(data);
     }).catch((err) => {
-        res.json({message:"no results"});
+        res.json({ message: "no results" });
     });
 });
 
-app.get("/tas", (req,res) => {
-    data.getTAs().then((data)=>{
+app.get("/tas", (req, res) => {
+    data.getTAs().then((data) => {
         res.json(data);
     });
 });
 
-app.get("/courses", (req,res) => {
-    data.getCourses().then((data)=>{
+app.get("/courses", (req, res) => {
+    data.getCourses().then((data) => {
         res.json(data);
     });
 });
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
 
-
-data.initialize().then(function(){
-    app.listen(HTTP_PORT, function(){
-        console.log("app listening on: " + HTTP_PORT)
+data.initialize().then(function () {
+    app.listen(HTTP_PORT, function () {
+        console.log("app listening on: " + HTTP_PORT);
     });
-}).catch(function(err){
+}).catch(function (err) {
     console.log("unable to start server: " + err);
 });
 
