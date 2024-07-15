543774df27fd797821fe2fe3cc0b9ad4f8c9467d
emadchy96 <echowdhury7@myseneca.ca>
2024-07-15 15:46:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname,"/public"));
+app.use(express.static("/public"));
 
 app.use(express.urlencoded({ extended: true }));
 
